22ef67fc76a3832f1133424620a16c951ed23bfe
onaxel <axel.s09kpo@gmail.com>
2024-05-30 00:27:20 -0300

    feat: add defaultCategories on first login

--- js/authentication.js
+++ js/authentication.js
@@ -15,6 +15,8 @@ import {
 // import {AWS_ACCESS_KEY,AWS_COGNITO_POOL_CLIENT_ID,AWS_COGNITO_POOL_ID,AWS_REGION,AWS_SECRET_ACCESS_KEY } from '../secrets'
 import { Amplify } from "aws-amplify";
 import backendConfig from "../src/amplifyconfiguration.json";
+import { client } from "./amplifyConfig.js";
+import { createDefaultCategories } from "../src/graphql/mutations.js";
 
 Amplify.configure(backendConfig);
 
@@ -50,18 +52,49 @@ export async function logout() {
 const confirmSignInWithNewPassword = async () => {
     const newPassword = prompt("Please enter a new password:"),
         name = prompt("Indica tu nombre"),
-        family_name = prompt("Indica tu apellido");
+        family_name = prompt("Indica tu apellido"),
+        defaultCategories = ["INBOX", "UNCATEGORY"];
     try {
         const data = await confirmSignIn({
-            challengeResponse: newPassword,
-            options: {
-                userAttributes: {
-                    name,
-                    family_name,
+                challengeResponse: newPassword,
+                options: {
+                    userAttributes: {
+                        name,
+                        family_name,
+                    },
+                    friendlyDeviceName: "kideChannel",
                 },
-                friendlyDeviceName: "kideChannel",
-            },
-        });
+            }),
+            userData = await getCurrentUser();
+
+        for (let index = 0; index < defaultCategories.length; index++) {
+            const element = defaultCategories[index];
+            try {
+                await client.graphql({
+                    query: createDefaultCategories,
+                    variables: {
+                        input: {
+                            clientId: userData.userId,
+                            categoryName: element,
+                            configuration: {
+                                autoRedirect: false,
+                                autoRetargeting: false,
+                                autoTrigger: false,
+                                autoQuote: false,
+                                autoResponse: false,
+                                redirectTo: "{}",
+                                quoteOption: "{}",
+                                triggerOption: "{}",
+                                retargetingOption: "{}",
+                                retargetingTime: "",
+                            },
+                        },
+                    },
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        }
         return "Successfully logged in";
     } catch (error) {
         throw new Error(`Error to set new password:${error.message}`);
@@ -109,4 +142,3 @@ export const getUserInfo = async () => {
         userData = await getCurrentUser();
     return { ...attributes, userData };
 };
-

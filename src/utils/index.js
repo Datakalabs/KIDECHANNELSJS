764cb3fadb81a6d3332830bfbe6866020662bad7
Axel-Dataka <axel.s@data-ka.com>
2024-08-15 11:42:12 -0300

    feat: update index

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,16 @@ export {
     fetchRetargetingOptions,
     fetchContacts,
     fetchTags,
+    getSync,
 } from "./fetchFunctions";
-
 export { getDefaultCategoriesConfiguration } from "./defaultCategories";
+export { createBadge } from "./tableFunctions";
+export {
+    createTag,
+    deleteTag,
+    executeResponse,
+    modifyTag,
+} from "./postFunctions";
+export { client } from "./amplifyConfig";
+export { getColorObj } from "./groupsUtils";
+export { awsDateTimeFormat, normalizeDate } from "./normalizeDateTime";
